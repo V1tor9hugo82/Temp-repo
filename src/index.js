1d24f755c674739193baea1a5e58ab9324b0c067
vitor hugo <vhrocha17@gmail.com>
2021-12-28 20:13:05 -0300

    enviando os primeiros commits do repositorio

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/global-style.css';
+
+
+import { Home } from './templates/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
